4e37df4479f9a379dfa283a55bbea28cef16606d
Fede <fede.garza.ramirez@gmail.com>
2021-09-23 13:04:14 -0500

    add parse_args

---
+++ api/utils/utils.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel 
+
+
+def parse_args(args: BaseModel):
+    dict_args = args.dict()
+
+    outputs_args = []
+
+    for key, value in dict_args.items():
+        if value is None:
+            continue
+
+        key = key.replace('_', '-')
+        key = f'--{key}'
+
+        outputs_args.append(key)
+        outputs_args.append(str(value))
+
+    return outputs_args
+
+if __name__ == '__main__':
+
+    args = BaseModel.construct(arg_1='arg1', arg_2='arg2', 
+                               argn='argn', arg_3=None)
+
+    expected = ['--arg-1', 'arg1', '--arg-2', 'arg2', 
+                '--argn', 'argn']
+
+    parsed_args = parse_args(args)
+
+    print(f'expected args: {expected}')
+    print(f'parsed args: {parsed_args}')
+
+    assert expected == parsed_args
