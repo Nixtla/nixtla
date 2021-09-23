d849a5ec76d2401f1b9cf904b761294ad943ef1a
Fede <fede.garza.ramirez@gmail.com>
2021-09-23 16:54:28 -0500

    add workflow template generation

---
+++ workflows.sh
@@ -0,0 +1,13 @@
+# read the workflow template
+WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yml)
+
+# iterate each route in routes directory
+for ROUTE in tsfeatures tsforecast tsbenchmarks tspreprocess; do
+    echo "generating workflow for ${ROUTE}"
+
+    # replace template route placeholder with route name
+    WORKFLOW=$(echo "${WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+
+    # save workflow to .github/workflows/{ROUTE}
+    echo "${WORKFLOW}" > .github/workflows/${ROUTE}.yml
+done
