cceef3d8577c5652ce4087ea82e12d2bbeb42bf9
Ubuntu <ubuntu@ip-172-31-30-245.ec2.internal>
2021-11-03 02:40:55 +0000

    add torch to pip and mod test

--- utils/docker/gpu/test.py
+++ utils/docker/gpu/test.py
@@ -5,4 +5,4 @@ assert torch.cuda.device_count() >= 1
 
 
 print(f'Device name: {torch.cuda.get_device_name(0)} \n')
-print(f'Current device: {torch.cuda.current_device(0)} \n')
+print(f'Current device: {torch.cuda.current_device()} \n')
