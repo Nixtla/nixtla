edbeccb0eac5c4fc27a93442db5fa822d1ec8ab0
KielRodriguez <kr.rdz.20@gmail.com>
2021-11-03 14:07:27 -0600

    config aws provisioner

---
+++ iac/terraform/aws/provisioner.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
