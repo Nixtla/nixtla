2eba42f360ee11459ce170833c89955f7d07bf13
KielRodriguez <kr.rdz.20@gmail.com>
2021-11-24 18:28:31 -0600

    fix: grant cloud watch logs permissions to sagemaker

--- iac/terraform/aws/modules/iam/main.tf
+++ iac/terraform/aws/modules/iam/main.tf
@@ -96,6 +96,15 @@ resource "aws_iam_policy" "sagemaker_policy" {
     Version = "2012-10-17"
     Statement = [
       {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
       Action = [
         "s3:PutObject",
         "s3:PutObjectAcl",
