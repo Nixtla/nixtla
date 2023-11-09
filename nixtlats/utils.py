ae6c1b41a117fcd98f6663eb1b8a2a1c9cba33fb
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-11-09 20:20:37 +0000

    fix: rm unwated message plot

--- nixtlats/utils.py
+++ nixtlats/utils.py
@@ -10,5 +10,5 @@ def colab_badge(path: str):
     base_url = "https://colab.research.google.com/github"
     badge_svg = "https://colab.research.google.com/assets/colab-badge.svg"
     nb_url = f"{base_url}/Nixtla/nixtla/blob/main/nbs/{path}.ipynb"
-    badge_md = f"[![Open In Colab]({badge_svg})]({nb_url})"
+    badge_md = f"[![]({badge_svg})]({nb_url})"
     display(Markdown(badge_md))
