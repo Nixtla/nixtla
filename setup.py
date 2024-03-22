986b95cce280a8f14e6bc21cb64f8a26d95e63df
FedericoGarza <fede.garza.ramirez@gmail.com>
2024-03-22 04:03:13 +0000

    v0.2.0

--- setup.py
+++ setup.py
@@ -3,14 +3,22 @@ import setuptools
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
-dev = ["black", "datasetsforecast", "nbdev", "plotly", "pre-commit", "python-dotenv", "statsforecast"]
+dev = [
+    "black",
+    "datasetsforecast",
+    "nbdev",
+    "plotly",
+    "pre-commit",
+    "python-dotenv",
+    "statsforecast",
+]
 distributed = ["dask[dataframe]", "fugue[ray]>=0.8.7", "pyspark", "ray[serve-grpc]"]
 plotting = ["utilsforecast[plotting]>=0.0.5"]
 date_extras = ["holidays"]
 
 setuptools.setup(
     name="nixtlats",
-    version="0.1.21",
+    version="0.2.0",
     description="TimeGPT SDK",
     long_description=long_description,
     long_description_content_type="text/markdown",
