cdde0e77a2e3d8c01695cf1cc441a17bb9e798be
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-04-24 16:26:11 +0000

    SDK regeneration

--- src/nixtlats/types/validation_error.py
+++ src/nixtlats/types/validation_error.py
@@ -3,14 +3,15 @@
 import datetime as dt
 import typing
 
-import pydantic
-
 from ..core.datetime_utils import serialize_datetime
-from .validation_error import ValidationError
+from ..core.pydantic_utilities import pydantic_v1
+from .validation_error_loc_item import ValidationErrorLocItem
 
 
-class HttpValidationError(pydantic.BaseModel):
-    detail: typing.Optional[typing.List[ValidationError]]
+class ValidationError(pydantic_v1.BaseModel):
+    loc: typing.List[ValidationErrorLocItem]
+    msg: str
+    type: str
 
     def json(self, **kwargs: typing.Any) -> str:
         kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
@@ -22,4 +23,6 @@ class HttpValidationError(pydantic.BaseModel):
 
     class Config:
         frozen = True
+        smart_union = True
+        extra = pydantic_v1.Extra.forbid
         json_encoders = {dt.datetime: serialize_datetime}
