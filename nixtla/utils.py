3ba3b37d689698b325fde527198de04c45cb3faf
Olivier Sprangers <osprangers@gmail.com>
2024-05-08 11:54:16 +0200

    try_with_not_run_colab

--- nixtla/utils.py
+++ nixtla/utils.py
@@ -12,3 +12,31 @@ def colab_badge(path: str):
     nb_url = f"{base_url}/Nixtla/nixtla/blob/main/nbs/{path}.ipynb"
     badge_md = f"[![]({badge_svg})]({nb_url})"
     display(Markdown(badge_md))
+
+# %% ../nbs/utils.ipynb 4
+import sys
+from contextlib import contextmanager
+
+# %% ../nbs/utils.ipynb 5
+class NoOpContext:
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return True
+
+
+@contextmanager
+def not_run_in_colab():
+    if "google.colab" in sys.modules:
+        yield NoOpContext()
+    else:
+        yield
+
+
+@contextmanager
+def only_run_in_colab():
+    if "google.colab" in sys.modules:
+        yield
+    else:
+        yield NoOpContext()
