37e7fc1777b94e39be6a7ecd598b041b0055fbb2
José Morales <jmoralz92@gmail.com>
2024-10-22 17:37:07 -0600

    fix(ci): increase tolerance in tests (#503)

--- nixtla/nixtla_client.py
+++ nixtla/nixtla_client.py
@@ -1628,7 +1628,7 @@ class NixtlaClient:
             ax=ax,
         )
 
-# %% ../nbs/src/nixtla_client.ipynb 54
+# %% ../nbs/src/nixtla_client.ipynb 50
 def _forecast_wrapper(
     df: pd.DataFrame,
     client: NixtlaClient,
