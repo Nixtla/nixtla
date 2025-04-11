df3261ac094ccb932b16afb08471006437fdd62d
Han Wang <goodwanghan@gmail.com>
2025-04-11 16:04:57 -0700

    Adjust model input size validation logic, add devcontainer (#632)

--- nixtla/nixtla_client.py
+++ nixtla/nixtla_client.py
@@ -1588,7 +1588,7 @@ class NixtlaClient:
         )
         standard_freq = _standardize_freq(freq, processed)
         model_input_size, model_horizon = self._get_model_params(model, standard_freq)
-        if finetune_steps > 0 or level is not None or add_history:
+        if finetune_steps > 0 or add_history:
             _validate_input_size(processed, model_input_size, model_horizon)
         if h > model_horizon:
             logger.warning(
