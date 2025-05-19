ef9ba8c8235e5d6d15c87dced49a3c9535d62ddc
t-minus <gohjingqiang@gmail.com>
2025-05-19 21:32:38 +0000

    include header information into the migrated tests

--- tests/migrate_tests.py
+++ tests/migrate_tests.py
@@ -6,7 +6,6 @@ pattern = r"(?ms)^#\s*\|\s*hide\s*\n.*?(?=^#\||^<VSCode\.Cell|\Z)"
 # Load the Jupyter Notebook
 notebook_filename = "/home/nixtla/nbs/src/nixtla_client.ipynb"
 new_notebook_filename = "/home/nixtla/nbs/src/new_nixtla_client.ipynb"
-# notebook_filename = 'nixtla_client.ipynb'
 with open(notebook_filename, 'r') as f:
     notebook_content = json.load(f)
 
@@ -16,19 +15,21 @@ new_cells = []
 
 # Iterate through the cells in the notebook
 for cell in notebook_content['cells']:
+    cell_text = "".join(cell['source'])
     if cell['cell_type'] == 'code':
         # Check if the cell contains a test function
-        cell_text = "".join(cell['source'])
         matches = re.findall(pattern, cell_text)
         if matches:
-        # if any(line.startswith('def test_') for line in cell['source']):
-            # test_functions.append('\n'.join(cell['source']))
             test_functions.append(cell_text)
         else:
             new_cells.append(cell)
+    else:
+        # Others such as markdown cells are added directly
+        test_functions.append(cell_text)
+        new_cells.append(cell)
 
 # Write the test functions to a new Python file
-with open('test_cases.py', 'w') as f:
+with open('/home/nixtla/tests/test_cases.py', 'w') as f:
     f.write('\n\n'.join(test_functions))
 
 # Update the notebook with the new cells
