dbd531439921a2c09726b1f43be6b6fbac21bb61
t-minus <gohjingqiang@gmail.com>
2025-05-20 21:29:52 +0000

    move fixtures and separate them into different files
    missed

---
+++ tests/fixtures/env.py
@@ -0,0 +1,15 @@
+import os
+from contextlib import contextmanager
+
+@contextmanager
+def delete_env_var(key):
+    original_value = os.environ.get(key)
+    rm = False
+    if key in os.environ:
+        del os.environ[key]
+        rm = True
+    try:
+        yield
+    finally:
+        if rm:
+            os.environ[key] = original_value
