4977e81a5a6140a671928744bafa3d83dbf279d0
t-minus <gohjingqiang@gmail.com>
2025-05-20 20:36:38 +0000

    test_date_features.py -> pytest framework

--- tests/date_features/test_date_features.py
+++ tests/date_features/test_date_features.py
@@ -1,23 +1,34 @@
 import pandas as pd
+import pytest
 
 from nixtla.date_features import CountryHolidays, SpecialDates
 
-def test_country_holidays_shape():
-    c_holidays = CountryHolidays(countries=['US', 'MX'])
-    periods = 365 * 5
-    dates = pd.date_range(end='2023-09-01', periods=periods)
-    holidays_df = c_holidays(dates)
-    assert len(holidays_df) == periods
+@pytest.fixture
+def periods():
+    return 365 * 5
 
-def test_special_dates_shape_and_sum():
-    special_dates = SpecialDates(
+@pytest.fixture
+def dates(periods):
+    return pd.date_range(end='2023-09-01', periods=periods)
+
+@pytest.fixture
+def country_holidays():
+    return CountryHolidays(countries=['US', 'MX'])
+
+@pytest.fixture
+def special_dates():
+    return SpecialDates(
         special_dates={
             'Important Dates': ['2021-02-26', '2020-02-26'],
             'Very Important Dates': ['2021-01-26', '2020-01-26', '2019-01-26']
         }
     )
-    periods = 365 * 5
-    dates = pd.date_range(end='2023-09-01', periods=periods)
+
+def test_country_holidays_shape(country_holidays, dates, periods):
+    holidays_df = country_holidays(dates)
+    assert len(holidays_df) == periods
+
+def test_special_dates_shape_and_sum(special_dates, dates, periods):
     holidays_df = special_dates(dates)
     assert len(holidays_df) == periods
     assert holidays_df.sum().sum() == 5
