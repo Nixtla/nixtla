f6bcee268d41c6bf5fed89f31992569915e9ce4a
Han Wang <han@nixtla.io>
2025-08-19 21:30:16 +0000

    refactor tests

---
+++ nixtla_tests/fixtures/spark_fixtures.py
@@ -0,0 +1,54 @@
+import pytest
+from pyspark.sql import SparkSession
+
+
+@pytest.fixture(scope="module")
+def spark_client():
+    with SparkSession.builder.getOrCreate() as spark:
+        yield spark
+
+
+@pytest.fixture(scope="module")
+def spark_df(spark_client, distributed_series):
+    spark_df = spark_client.createDataFrame(distributed_series).repartition(2)
+    return spark_df
+
+
+@pytest.fixture(scope="module")
+def spark_diff_cols_df(spark_client, distributed_series, renamer):
+    spark_df = spark_client.createDataFrame(
+        distributed_series.rename(columns=renamer)
+    ).repartition(2)
+    return spark_df
+
+
+@pytest.fixture(scope="module")
+def spark_df_x(spark_client, distributed_df_x):
+    spark_df = spark_client.createDataFrame(distributed_df_x).repartition(2)
+    return spark_df
+
+
+@pytest.fixture(scope="module")
+def spark_df_x_diff_cols(spark_client, distributed_df_x, renamer):
+    spark_df = spark_client.createDataFrame(
+        distributed_df_x.rename(columns=renamer)
+    ).repartition(2)
+    return spark_df
+
+
+@pytest.fixture(scope="module")
+def spark_future_ex_vars_df(spark_client, distributed_future_ex_vars_df):
+    spark_df = spark_client.createDataFrame(distributed_future_ex_vars_df).repartition(
+        2
+    )
+    return spark_df
+
+
+@pytest.fixture(scope="module")
+def spark_future_ex_vars_df_diff_cols(
+    spark_client, distributed_future_ex_vars_df, renamer
+):
+    spark_df = spark_client.createDataFrame(
+        distributed_future_ex_vars_df.rename(columns=renamer)
+    ).repartition(2)
+    return spark_df
