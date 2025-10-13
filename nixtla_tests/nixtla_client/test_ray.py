c01c3b31bead2d731b9498b54c5b01cb41501878
Jing-Qiang (JQ) Goh <gohjingqiang@gmail.com>
2025-10-13 18:14:18 +0800

    [FEAT] model_parameters argument (#681)

--- nixtla_tests/nixtla_client/test_ray.py
+++ nixtla_tests/nixtla_client/test_ray.py
@@ -1,18 +1,24 @@
+import platform
+import sys
+
 import pytest
 
-from nixtla_tests.helpers.checks import check_anomalies_dataframe
-from nixtla_tests.helpers.checks import check_anomalies_online_dataframe
-from nixtla_tests.helpers.checks import check_anomalies_dataframe_diff_cols
-from nixtla_tests.helpers.checks import check_forecast_dataframe
-from nixtla_tests.helpers.checks import check_forecast_dataframe_diff_cols
-from nixtla_tests.helpers.checks import check_forecast_x_dataframe
-from nixtla_tests.helpers.checks import check_forecast_x_dataframe_diff_cols
-from nixtla_tests.helpers.checks import check_quantiles
+from nixtla_tests.helpers.checks import (
+    check_anomalies_dataframe,
+    check_anomalies_dataframe_diff_cols,
+    check_anomalies_online_dataframe,
+    check_forecast_dataframe,
+    check_forecast_dataframe_diff_cols,
+    check_forecast_x_dataframe,
+    check_forecast_x_dataframe_diff_cols,
+    check_quantiles,
+)
 
 pytestmark = [
     pytest.mark.distributed_run,
-    pytest.mark.ray_run
+    pytest.mark.ray_run,
 ]
+
 
 def test_quantiles(nixtla_test_client, ray_df):
     check_quantiles(nixtla_test_client, ray_df, id_col="unique_id", time_col="ds")
@@ -33,7 +39,12 @@ def test_anomalies(nixtla_test_client, ray_df, ray_diff_cols_df):
 def test_anomalies_online(nixtla_test_client, ray_df):
     check_anomalies_online_dataframe(nixtla_test_client, ray_df)
 
-
+@pytest.mark.xfail(
+    reason=(
+        "triad.collections.schema.SchemaError: Schema can't be empty"
+        "error triggered https://github.com/Nixtla/nixtla/blob/b56a89bf6b80b137c57f3511eef3ed8857705a59/nixtla/nixtla_client.py#L1383"
+    )
+)
 def test_forecast_x_dataframe(
     nixtla_test_client,
     ray_df_x,
